aa4ed8c4cc9a324baf4f37d4756132e067bb6a20
Vitas <twandy543hient@gmail.com>
2025-03-06 13:26:13 +0800

    Update chatGPT.ts

--- src/chatGPT.ts
+++ src/chatGPT.ts
@@ -1,10 +1,10 @@
 import { AzureOpenAI } from 'openai';
 import { ChatCompletion } from 'openai/resources/index.mjs';
 
-const endpoint = process.env["AZURE_OPENAI_ENDPOINT"] || "https://vitas-m7viocp8-eastus2.openai.azure.com/";  
+const endpoint = process.env["AZURE_OPENAI_ENDPOINT"] || "";  
 const apiVersion = "2024-05-01-preview";
 const deployment = "gpt-4o";
-const apiKey = process.env["AZURE_OPENAI_API_KEY"] || "864HXGx892NdVu196eSHZTDNAfDLYogNaJQfdZWSpPBvGyOhQnOlJQQJ99BCACHYHv6XJ3w3AAAAACOGRuAM";  
+const apiKey = process.env["AZURE_OPENAI_API_KEY"] || "";  
 const openai = new AzureOpenAI({ endpoint, apiKey, apiVersion, deployment, dangerouslyAllowBrowser: true });  
 
 export const chatWithGPT = async (message: string) => {
